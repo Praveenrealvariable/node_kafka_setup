ce243fad5ac291d7fde8692c0f0de3c8c3f62996
Praveen K <kodadala.praveen@realvariable.com>
2023-05-01 11:13:29 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+--> install docker desktop
+
+--> install wsl_update_x64.msi  for windows
+
+--> docker-compose up -d  (to start docker)
+
+
 Read messages directly from kafka
 --sh     open shell  -->
 docker-compose exec kafka sh
@@ -11,7 +18,5 @@ cd /user/bin
 ./kafka-console-consumer --topic test-topic --boostrap-server localhost:9092 --from beginning
 
 
-## install docker desktop
 
-## install wsl_update_x64.msi  for windows
 
--- index.js
+++ index.js
@@ -1,47 +1,105 @@
 const { Kafka } = require('kafkajs')
-
-const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['localhost:9092'],
-})
-
-const producerFunc = async()=>{
-
-const producer = kafka.producer()
-await producer.connect()
- const sendFunc = await producer.send({
-  topic: 'test-topic',
-  messages: [
-    { value: 'Hello KafkaJS user!' },
-  ],
-})
+const express = require('express')
+const app = express()
 
 
-console.log("sender sending msgs in half sec")
-//await producer.disconnect()
-
-
+function connectToKafka() {
+  const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092'],
+  })
+  return kafka
 }
-
-setInterval(producerFunc, 500)
-
-const consumerFunc = async()=>{
-
-const consumer = kafka.consumer({ groupId: 'test-group' })
-
-await consumer.connect()
-await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
-
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log("receiver receiving msges in half sec")
-    console.log({
-      value: message.value.toString(),
+async function producerFunc() {
+  try{
+    return new Promise(async (resolve, reject) => {
+      let kafkaProducer = connectToKafka()
+      const producer = kafkaProducer.producer()
+      await producer.connect().then(() => {console.log('kafka Producer connected')})
+      .catch(e => {throw new Error('Could not connect producer')});
+      await producer.send({
+        topic: 'test-topic',
+        messages: [
+          { value: 'Hello KafkaJS user this is message from Producer !' },
+        ],
+      })
+      //await producer.disconnect()
+      let consumerResult = await consumerFunc()
+      resolve(consumerResult)
     })
-  },
-})
+  }catch(error){
+    console.log('error', error)
+   const err = new Error('Oops! Something went wrong in Producer.');   
+   err.statusCode = 500; 
+   throw err;
+  }
+
+}
+async function consumerFunc() {
+  try{
+    return new Promise(async (resolve, reject) => {
+      connectToKafka()
+      let kafkaConsumer = connectToKafka()
+      const consumer = kafkaConsumer.consumer({ groupId: 'test-group' })
+      await consumer.connect().then(() => {console.log('kafka consumer connected')})
+      .catch(e => {throw new Error('Could not connect consumer')});
+      await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+      await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          // console.log({value: message.value.toString()})
+          resolve({ value: message.value.toString() })
+        },
+      })
+    })
+  }catch(error){
+    console.log(error)
+    const err = new Error('Oops! Something went wrong in consumer.');   
+    err.statusCode = 500; 
+    throw err;
+  }
 
 }
 
-consumerFunc()
+app.get('/kafka', async (req, res, next) => {
+  try{
+    connectToKafka()
+    let result = await producerFunc()
+    res.status(200).json({status:true,data:result})
+  }catch(error){
+    next(error)    
+  }
+})
+
+app.get('/', function(req, res, next) {
+  Promise.resolve().then(() => {
+      const rand = Math.random()
+      if (rand < 0.5) {
+        const err = new Error(`Oops! Something went wrong ${String.fromCodePoint(0x1F602)}.`);
+        err.statusCode = 400;
+        throw err;
+      }
+      res.status(200).json({status:true,data:'Hello, World!'});
+    })
+    .catch((err) => {
+      console.log('err', err)
+      next(err);
+    });
+});
+
+
+// Error handler middleware function
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Something went wrong';
+
+  res.status(statusCode).json({
+    errorStatus:true,
+    statusCode: statusCode,
+    message: message,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000!')
+})
 
